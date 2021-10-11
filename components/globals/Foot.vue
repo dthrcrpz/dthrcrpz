b296a73919381539c7d39e1914727e842abb7b72
teryong <dthrcrpz@gmail.com>
2021-10-11 18:01:59 +0800

    done with footer

--- components/globals/Foot.vue
+++ components/globals/Foot.vue
@@ -1,14 +1,41 @@
 <template>
     <footer>
         <div class="container">
-            sadsad
+            <div class="plugs">
+                <a href="https://nuxtjs.org/" target="_blank">
+                    <img src="/images/footer/plugs/nuxt.svg" alt="">
+                </a>
+                <a class="netlify" href="https://www.netlify.com/" target="_blank">
+                    <img src="/images/footer/plugs/netlify.svg" alt="">
+                </a>
+            </div>
+            <div class="copyright">
+                Copyright © 2021. All Rights Reserved.
+            </div>
         </div>
     </footer>
 </template>
 
 <style lang="sass" scoped>
     footer
-        background-color: #22b4ab
-        padding: 20px 0
-        color: red
+        padding-bottom: 30px
+        .land
+            width: 100%
+        .container
+            .plugs
+                display: flex
+                width: 100%
+                justify-content: center
+                margin-bottom: 20px
+                align-items: center
+                a
+                    max-width: 100px
+                    width: 100%
+                    margin: 0 10px
+                    &.netlify
+                        max-width: 80px
+                    img
+                        width: 100%
+            .copyright
+                text-align: center
 </style>
