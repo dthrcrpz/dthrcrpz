55d559003a4b4e549045a4b911cb567ab2871c64
dthrcrpz <dthrcrpz@gmail.com>
2022-01-12 22:05:35 +0800

    convert footer

--- components/globals/Foot.vue
+++ components/globals/Foot.vue
@@ -1,9 +1,9 @@
 <template>
     <footer class="pb-8">
-        <div class="container">
+        <div class="cntnr">
             <p class="powered-by mt-0 mx-auto mb-3 text-center text-sm font-fira-code">This website is powered by:</p>
             <div class="plugs flex w-full justify-center mb-5 items-center">
-                <a rel="noopener" href="https://nuxtjs.org/" target="_blank" class="min-w-full w-full my-0 mx-3 duration-500 hover:scale-[1.1]">
+                <a rel="noopener" href="https://nuxtjs.org/" target="_blank" class="max-w-[100px] w-full my-0 mx-3 duration-500 hover:scale-[1.1]">
                     <img src="/images/footer/plugs/nuxt.svg" alt="" class="w-full">
                 </a>
             </div>
