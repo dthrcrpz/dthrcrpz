1d2ac6e43732f15d35de73f0bfb4395efc677e6c
dthrcrpz <dthrcrpz@gmail.com>
2022-03-29 17:33:26 +0800

    start ui kit

---
+++ src/routes/ui-kit.svelte
@@ -0,0 +1,30 @@
+<div class="container mx-auto mt-10">
+  <p class="text-5xl font-bold text-cyan-500">Heading Monteserrat</p>
+  <p class="text-5xl font-bold text-orange-600 font-fira-code">Heading Fira Code</p>
+
+  <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque quia repellat tempora sint, distinctio alias architecto? Voluptate culpa id ut quas. Sit error natus tenetur autem harum dolore laborum.
+  Vel, similique accusamus aliquam at <b>recusandae</b> <span class="font-bold">consectetur</span> facilis? Labore consequuntur necessitatibus, error eligendi laboriosam provident corrupti dolorum pariatur magni a sint accusamus ratione, aliquam temporibus veritatis ex laborum cumque nemo.
+  Doloribus distinctio eos, consequuntur deleniti eum sint.</p>
+
+  <div class="mt-10">
+    <h1 class="text-3xl mb-3">Buttons</h1>
+    <button class="block mb-5 font-semibold shadow-md px-5 py-3 w-40 rounded-full text-white bg-cyan-500">Primary</button>
+    <button class="block mb-5 font-semibold shadow-md px-5 py-3 w-40 rounded-full text-white bg-indigo-700">Secondary</button>
+    <button class="block mb-5 font-semibold shadow-md px-5 py-3 w-40 rounded-full text-white bg-orange-600">Tertiary</button>
+    <button class="block mb-5 font-semibold shadow-md px-5 py-3 w-40 rounded-full text-white bg-rose-700">Danger</button>
+  </div>
+
+  <div class="mt-10">
+    <h1 class="text-3xl mb-3">Form</h1>
+    <div class="mb-5">
+      <label class="block mb-2">Textbox: <span class="text-orange-600">*</span></label>
+      <input type="text" placeholder="Your email address" class="w-96 px-5 py-3 rounded-full text-slate-900 outline-none font-semibold border-2 border-blue-700">
+    </div>
+    <div class="mb-5">
+      <label class="block mb-2">Dropdown: <span class="text-orange-600">*</span></label>
+      <select class="w-96 px-5 py-3 rounded-full text-slate-900 outline-none font-semibold border-2 border-blue-700">
+        <option>Select one</option>
+      </select>
+    </div>
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,16 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
-        'teal': '#5CE0D8',
-        'dark-teal': '#22b4ab',
-        'dark-blue': '#1A2238',
-        'yellow': '#FFCF43',
-        'red': '#ff5757'
-      },
+      // colors: {
+      //   'teal': '#22b4ab',
+      //   'dark-blue': '#1A2238',
+      //   'yellow': '#FFCF43',
+      //   'red': '#ff5757'
+      // },
       fontFamily: {
-        'ubuntu': ['Ubuntu', 'sans-serif']
+        'fira-code': ['Fira Code', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
       },
-      boxShadow: {
-        'teal': '0px 0px 7px rgba(92, 224, 216, 0.3)',
-        'teal-glow': '0px 0px 10px rgba(92, 224, 216, 0.6)' 
-      }
     },
   },
   darkMode: 'class',
