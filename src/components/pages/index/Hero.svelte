d32b44651a490ee3548fc6035b6e7402b3a4fa23
dthrcrpz <dthrcrpz@gmail.com>
2022-04-03 13:23:52 +0800

    [Hero] add face

--- src/components/pages/index/Hero.svelte
+++ src/components/pages/index/Hero.svelte
@@ -3,6 +3,38 @@
     texts here
   </div>
   <div class="self-center my-auto">
-    icon here
+    <svg width="360" height="270" viewBox="0 0 360 270" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="Face">
+      <g id="eyes and glasses">
+      <g id="glasses" filter="url(#filter0_d_10_50)">
+      <path id="glass-left" class="transition-colors duration-300 stroke-dark dark:stroke-white" d="M169.899 35.19C163.388 36.3772 19.5162 17.8179 12.0037 21.9733C4.49112 26.1287 7.72778 75.3656 20.2277 94.8656C32.7277 114.366 102.557 123.475 135.728 112.366C168.899 101.256 176.409 34.0027 169.899 35.19Z" stroke-width="8"/>
+      <path id="glass-right" class="transition-colors duration-300 stroke-dark dark:stroke-white" d="M189.367 35.19C195.878 36.3772 339.75 17.8179 347.262 21.9733C354.775 26.1287 351.538 75.3656 339.038 94.8656C326.538 114.366 256.709 123.475 223.538 112.366C190.367 101.256 182.856 34.0027 189.367 35.19Z" stroke-width="8"/>
+      <path id="glass-middle" class="transition-colors duration-300 fill-dark stroke-dark dark:stroke-white dark:fill-white" d="M187.384 35.3656H173.004C170.437 35.3656 175.059 48.3656 175.059 48.3656C175.059 48.3656 178.728 47.3656 180.728 47.3656C182.728 47.3656 185.843 48.3656 185.843 48.3656C185.843 48.3656 189.951 35.3656 187.384 35.3656Z" stroke-width="8"/>
+      </g>
+      <g id="eyes">
+      <path id="eye-right" class="fill-primary" d="M225.723 60.627C225.723 60.627 264.201 62.231 277.624 59.5057C291.046 56.7804 301.8 43.9822 301.8 43.9822C301.8 43.9822 273.367 41.3784 260.648 44.629C247.93 47.8795 225.723 60.627 225.723 60.627Z"/>
+      <path id="eye-left" class="fill-primary" d="M139.848 65.2785C139.848 65.2785 97.258 69.7289 82.9537 66.3996C68.6493 63.0703 58.9769 44.6277 58.9769 44.6277C58.9769 44.6277 90.688 39.231 104.129 43.3787C117.57 47.5265 139.848 65.2785 139.848 65.2785Z"/>
+      </g>
+      </g>
+      <path id="nose" class="transition-colors duration-300 stroke-dark dark:stroke-white" d="M164.5 71.3656C164.5 89.3656 147.955 112.059 157.787 117.698C167.618 123.337 169.99 127.366 177.715 127.366C185.44 127.366 189.953 121.726 203.296 117.698" stroke-width="3"/>
+      <path id="face mask" class="transition-colors duration-300 stroke-dark dark:stroke-white fill-dark dark:fill-white" d="M131.586 113.937C123.069 113.937 22.8331 123.866 19.8653 129.427C16.8975 134.988 28.3821 183.876 60.9465 219.08C93.5108 254.284 138.099 268.366 175.172 268.366C212.246 268.366 248.29 257.743 287.895 223.304C327.5 188.866 341 135.06 341 129.427C341 123.794 238.297 114.876 226.273 113.937C214.25 112.998 189.2 107.366 175.172 107.366C161.145 107.366 140.103 113.937 131.586 113.937Z" stroke-width="3"/>
+      <g id="eyebrows">
+      <path id="eyebrow-left" class="transition-colors duration-300 stroke-dark dark:stroke-white fill-dark dark:fill-white" d="M56.5 14.3656C64 15.8656 158 52.8656 158 52.8656C158 52.8656 72.5 4.04559 65.5 2.3656C58.5 0.685616 32.5 15.8655 32.5 15.8655C32.5 15.8655 49 12.8656 56.5 14.3656Z"/>
+      <path id="eyebrow-right" class="transition-colors duration-300 stroke-dark dark:stroke-white fill-dark dark:fill-white" d="M301 11.3151C293.5 12.8151 210 58.3151 210 58.3151C210 58.3151 277.5 4.3656 290 1.3656C302.5 -1.6344 325.5 14.8151 325.5 14.8151C325.5 14.8151 308.5 9.81506 301 11.3151Z"/>
+      </g>
+      </g>
+      <defs>
+      <filter id="filter0_d_10_50" x="9.53674e-06" y="17.3656" width="359.266" height="111.806" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="2"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_10_50"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_10_50" result="shape"/>
+      </filter>
+      </defs>
+    </svg>
   </div>
 </div>
