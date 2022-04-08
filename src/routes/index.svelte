b08f01a4cdddd2ee9dd895deb33eb2cfa63d098f
dthrcrpz <dthrcrpz@gmail.com>
2022-04-09 00:31:08 +0800

    adjust svg backgrounds, add technicalities section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <div class="page-index">
   <Hero/>
   <WhatIDo/>
+  <Technicalities/>
 </div>
 
 <script>
   import Hero from "../components/pages/index/Hero.svelte";
   import WhatIDo from "../components/pages/index/WhatIDo.svelte";
+  import Technicalities from "../components/pages/index/Technicalities.svelte";
 </script>
