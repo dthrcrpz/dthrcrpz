6ae41a0956f769b0afc3cde16e4eea5a6cccbfdb
dthrcrpz <dthrcrpz@gmail.com>
2022-04-30 02:21:51 +0800

    css fixes

--- src/components/pages/index/Projects.svelte
+++ src/components/pages/index/Projects.svelte
@@ -26,7 +26,7 @@
       {#each projects as project}
         <div class="w-full md:w-1/2 py-4 px-0 md:px-4">
           <div class="rounded-2xl relative group flex-col transition-shadow duration-300 shadow-[0px_1px_8px_0px_#00000020] dark:shadow-[0px_1px_8px_0px_#06b6d430]">
-            <a href={project.agency.url} target="_blank" class="agency absolute max-w-[120px] w-full shadow-sm hover:shadow-md hover:shadow-primary shadow-primary transition-all duration-300 opacity-1 xl:opacity-0 group-hover:opacity-100 bg-dark px-3 py-2 rounded-xl z-[2] top-0 left-5 -translate-y-1/2">
+            <a href={project.agency.url} target="_blank" class="agency absolute max-w-[85px] md:max-w-[120px] w-full shadow-sm hover:shadow-md hover:shadow-primary shadow-primary transition-all duration-300 opacity-1 xl:opacity-0 group-hover:opacity-100 bg-dark px-3 py-2 rounded-xl z-[2] top-0 left-5 -translate-y-1/2">
               <LazyImage
                 src="/images/projects/agencies/{ project.agency.logo }"
                 placeholder="/images/extras/loading.svg"
@@ -41,7 +41,7 @@
                 class="image-cover"
               />
             </div>
-            <div class="flex flex-col p-5 xl:hidden">
+            <div class="flex flex-col p-5 xl:hidden bg-white dark:bg-dark rounded-b-xl">
               <p class="font-semibold mb-5 text-lg text-dark dark:text-white">{ project.name }</p>
               <a href={project.url} target="_blank" class="btn btn-primary has-image">
                 <span>VISIT WEBSITE</span>
