971b28ae8eb45ad037999852e476c176c665093e
dthrcrpz <dthrcrpz@gmail.com>
2022-04-30 02:49:20 +0800

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.my-site.dev',
+  site: 'https://dthrcrpz.dev',
   integrations: [
     tailwind(),
-    svelte()
+    svelte(),
+    sitemap()
   ]
 });
